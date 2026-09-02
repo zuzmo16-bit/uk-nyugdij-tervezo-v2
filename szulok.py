c891d95f911c1e348558041262ba231b98fb743b
zuzmo16-bit <zuzmo16@gmail.com>
2026-09-02 20:00:46 +0100

    Update szulok.py

--- szulok.py
+++ szulok.py
@@ -221,3 +221,4 @@ if user_mode == "Céges igazgató / Vállalkozó":
     
     col_dir1, col_dir2 = st.columns(2)
     col_dir1.success(f"💰 **A céged által megspórolt Társasági adó (Corporation Tax):** £{corporation_tax_saved:,.2f}")
+st.plotly_chart(fig, use_container_width=True, theme=None)
